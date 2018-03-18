daf035850a71e8707a1eda7af5449e2394b3cd22
Brian Bycynski <high5bri@hotmail.com>
2018-03-18 05:50:45 -0400

    finished lecture 56

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -2,26 +2,35 @@ import React, { Component } from "react";
 
 import axios from "axios";
 
+import SliderTemplates from "./slider_templates";
+import { URL } from "../../../config";
+
 class NewsSlider extends Component {
     state = {
         news: []
     };
 
     componentWillMount() {
-        axios.get(`http://localhost:3004/articles`).then(response => {
-            this.setState({
-                news: response.data
+        axios
+            .get(
+                `${URL}/articles?_start=${this.props.start}&_end=${
+                    this.props.amount
+                }`
+            )
+            .then(response => {
+                this.setState({
+                    news: response.data
+                });
             });
-        });
     }
 
     render() {
-        console.log(this.state.news);
-
         return (
-            <div>
-                <h1>Slider</h1>
-            </div>
+            <SliderTemplates
+                data={this.state.news}
+                type={this.props.type}
+                settings={this.props.settings}
+            />
         );
     }
 }
